fa4e02cee663980a837b77e0a5d4075e7ff9a832
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-03-25 16:11:11 +0530

    Add files via upload

--- frontend/src/pages/seller/SellerDashboard.jsx
+++ frontend/src/pages/seller/SellerDashboard.jsx
@@ -91,7 +91,7 @@ const SellerDashboard = () => {
                             >
                                 <LocalMallIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
 
-                                SHOPCART
+                                FLASHBUY
                             </NavLogo>
                         </Typography>
 
@@ -122,7 +122,7 @@ const SellerDashboard = () => {
                             >
                                 <LocalMallIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
 
-                                SHOPCART
+                                FLASHBUY
                             </NavLogo>
                         </Typography>
 
